799d051afceab6214d42b4f9bc48557b008ad989
Himanshu Shankar <amhssingh@gmail.com>
2017-10-16 11:55:49 +0530

    Fixed some errors

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ class HSPConnector:
     key_query = '?'
     key_query_separater = '&'
     key_msgid = 'msgid'
+    msgid = None
+    response = None
 
     def __init__(self, username: str, apikey: str, sender: str, smstype: str):
         self.username = username
@@ -21,7 +23,7 @@ class HSPConnector:
         self.sender = sender
         self.sms_type = smstype
 
-    def send_request(self, url:str):
+    def send_request(self, url: str):
         import requests
 
         self.response = requests.get(url)
@@ -38,7 +40,7 @@ class HSPConnector:
         if scheduled:
             params[self.key_scheduled] = scheduled
 
-        self.send_request(self.base_url + url + self.key_query + parse.urlencode(params) + self.key_numbers + '='
+        self.send_request(self.base_url + url + self.key_query + parse.urlencode(params) + '&' + self.key_numbers + '='
                           + ','.join(recipient))
 
         return self.parse_response()
@@ -55,12 +57,20 @@ class HSPConnector:
         return self.parse_response()
 
     def parse_response(self):
+        import logging
+
+        logger = logging.getLogger(__name__)
+
         if 200 <= self.response.status_code < 300:
             data = self.response.json()
-            for some_data in self.response.json():
+            for some_data in data:
                 if 'msgid' in some_data.keys():
                     self.msgid = some_data['msgid']
                     return self.msgid
+            logger.error('MSGID was not found! ' + str(self.response.text) + '. Status Code: '
+                         + str(self.response.status_code))
             raise ConnectionError('MSGID not found')
         else:
+            logger.error('MSGID was not found! ' + str(self.response.text) + '. Status Code: '
+                         + str(self.response.status_code))
             raise ConnectionError('Error code responded')
